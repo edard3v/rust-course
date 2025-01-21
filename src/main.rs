3c8e21ad2af917ef6ade04a40d759fc3762bbd84
edard3v <edard3v@gmail.com>
2025-01-21 02:57:37 -0500

    let y const

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("¡Hola mundo!")
+    let mut username = "edard3v";
+    let age = 29;
+    println!("Hola soy {} y tengo {} años.", username, age);
+
+    username = "asdfasdf";
+    println!("{}", username);
+
+    const PI: f64 = 3.1416;
+    println!("{}", PI)
 }
