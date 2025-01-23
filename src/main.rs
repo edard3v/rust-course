b9352d2b3fcea900febe08a8abbe4bd80161812a
edard3v <edard3v@gmail.com>
2025-01-23 02:00:38 -0500

    funciones

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,14 @@
-fn main() {}
+fn main() {
+    let x = add(5.5, 6.0);
+    let y = subtract(5.5, 6.0);
+
+    println!("{x} {y}");
+}
+
+fn add(a: f64, b: f64) -> f64 {
+    return a + b;
+}
+
+fn subtract(a: f64, b: f64) -> f64 {
+    return a - b;
+}
