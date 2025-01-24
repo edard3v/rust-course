6b6698b4f2d0bf3fbce318ccf3261ab9c4fe9e8c
edard3v <edard3v@gmail.com>
2025-01-24 13:30:31 -0500

    vectores

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,14 @@
-fn main() {}
+fn main() {
+    let mut a = vec![1, 2];
+
+    println!("{a:?}");
+
+    a.insert(1, 9);
+    println!("{a:?}");
+
+    a.pop();
+    println!("{a:?}");
+
+    a.remove(1);
+    println!("{a:?}");
+}
