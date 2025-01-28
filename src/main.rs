66994f6a6acd38525943b8dbf9d1a9b75f249839
edard3v <edard3v@gmail.com>
2025-01-28 03:26:20 -0500

    struct

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,27 @@
-fn main() {}
+fn main() {
+    #[derive(Debug)]
+    #[allow(dead_code)]
+    struct Account {
+        username: String,
+        password: String,
+    }
+
+    impl Account {
+        fn new(username: &str, password: &str) -> Self {
+            Account {
+                username: username.to_string(),
+                password: password.to_string(),
+            }
+        }
+
+        fn change_password(&mut self, new_password: &str) {
+            self.password = new_password.to_string()
+        }
+    }
+
+    let mut edar = Account::new("edar", "123");
+
+    edar.change_password("asdkfasklfd");
+
+    println!("{edar:#?}");
+}
