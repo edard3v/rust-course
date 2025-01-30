4d693c86e3f8140f8e5e6ebfd7f44edcc34455f5
edard3v <edard3v@gmail.com>
2025-01-30 03:17:05 -0500

    match

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,14 @@
-fn main() {}
+fn main() {
+    let xy = (0, 0);
+    match xy {
+        (x @ 0, 0) => {
+            println!("Este es el punto de origen. {x}")
+        }
+        (..=-1, 1..) => {
+            println!("Este punto está en el segundo cuadrante.")
+        }
+        _ => {
+            println!("Ni idea en que cuadrante estás.")
+        }
+    }
+}
