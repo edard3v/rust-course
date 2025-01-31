47039a28d96e61887a6562bc1e18ec7a12da86c4
edard3v <edard3v@gmail.com>
2025-01-31 01:42:06 -0500

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,1 @@
-fn main() {
-    let xy = (0, 0);
-    match xy {
-        (x @ 0, 0) => {
-            println!("Este es el punto de origen. {x}")
-        }
-        (..=-1, 1..) => {
-            println!("Este punto está en el segundo cuadrante.")
-        }
-        _ => {
-            println!("Ni idea en que cuadrante estás.")
-        }
-    }
-}
+fn main() {}
