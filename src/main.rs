b2eca0254c79ff1ec67f7a31d29142867bd77911
edard3v <edard3v@gmail.com>
2025-01-31 22:43:50 -0500

    save

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,15 @@
-fn main() {}
+fn main() {
+    #[derive(Debug)]
+    #[expect(dead_code)]
+    enum Role {
+        Admin = 0,
+        Client = 99,
+        Boss,
+    }
+
+    fn print_role(role: Role) {
+        println!("{:?}", role as u32)
+    }
+
+    print_role(Role::Admin);
+}
