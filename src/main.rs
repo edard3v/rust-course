f5e678edcb90cf435f93090d351cc0611c207f59
edard3v <edard3v@gmail.com>
2025-02-02 14:16:45 -0500

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 fn main() {
-    let x = divide(4, 0);
+  let x = divide(4, 0);
 
-    match x {
-        Err(x) => println!("{x}"),
-        Ok(x) => println!("{x}"),
-    }
+  match x {
+    Err(x) => println!("{x}"),
+    Ok(x) => println!("{x}"),
+  }
 }
 
 fn divide(a: u32, b: u32) -> Result<u32, String> {
-    if b == 0 {
-        Err("No es posible dividir por 0".to_string())
-    } else {
-        Ok(a / b)
-    }
+  if b == 0 {
+    Err("No es posible dividir por 0".to_string())
+  } else {
+    Ok(a / b)
+  }
 }
