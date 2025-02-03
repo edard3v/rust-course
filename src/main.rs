82b1df0ad8d4495a4a52db8c5b5e5b76202ebdae
edard3v <edard3v@gmail.com>
2025-02-03 04:30:15 -0500

    empezando con lifetimes

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,10 @@
 fn main() {
-  let x = divide(4, 0);
+  let x = "EDAR".to_string();
+  let y = &x;
 
-  match x {
-    Err(x) => println!("{x}"),
-    Ok(x) => println!("{x}"),
-  }
+  foo(y);
 }
 
-fn divide(a: u32, b: u32) -> Result<u32, String> {
-  if b == 0 {
-    Err("No es posible dividir por 0".to_string())
-  } else {
-    Ok(a / b)
-  }
+fn foo(a: &str) -> &str {
+  a
 }
